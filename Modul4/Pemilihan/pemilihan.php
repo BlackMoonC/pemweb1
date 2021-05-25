0a9f3c8fb798b04d462141a25d962b07ec6e3c4d
BlackMoonC <51168038+BlackMoonC@users.noreply.github.com>
2021-05-25 09:39:38 +0700

    Update pemilihan.php

--- Modul4/Pemilihan/pemilihan.php
+++ Modul4/Pemilihan/pemilihan.php
@@ -1,4 +1,24 @@
-<?php
-include("index.php");
+<html>
 
-?>
+<head>
+    <title>Pemilihan</title>
+</head>
+
+<body>
+    <form action="hasil.php" method="POST">
+        <p>Tentukan pilihan untuk menjadi pemimpin selanjutnya</p>
+        <br>        <select name="name">
+            <option value="Ippo">1. Ippo</option>
+            <option value="Naruto">2.Naruto</option>
+            <option value="Sasuke">3.Sasuke</option>
+            <option value="Madara">4.Madara</option>
+            <option value="Hashirama">5.Hashirama</option>
+        </select>
+        <br>
+        <label> NIK</label>
+        <input type="number" name="NIK" />
+        <button type="submit">kirim</button>
+    </form>
+</body>
+
+</html>
